ad7dc6a2201c09fedad7810821fd10d299dbf9bc
Guilherme Carmona <guilherme.carmona@outlook.com>
2019-11-11 14:33:39 +0000

    basic working, just make it pretty

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,38 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import MainNavbar from './MainNavbar'
+// import MainNavbar from './MainNavbar'
 import Home from './pages/Home'
 import Countries from './pages/Countries'
 import AddCountry from './pages/AddCountry'
 import Secret from './pages/Secret'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import Countdown from 'react-countdown-now'
 
 export default function App() {
+  const Completionist = () => <span>TIME TO COME BACK BRIDGETTE!</span>
+  const endDate = new Date('2020-01-29T03:24:00')
+  // var date2 = new Date('1995-12-17T03:24:00');
+  // Sun Dec 17 1995 03:24:00 GMT...
+
+  const renderer = ({ days, hours, minutes, seconds, completed }) => {
+    if (completed) {
+      // Render a completed state
+      return <Completionist />
+    } else {
+      // Render a countdown
+      return (
+        <span>
+          Days: {days} Hours:{hours} Minutes: {minutes} Seconds:{seconds}
+        </span>
+      )
+    }
+  }
   return (
     <div className="App">
-      <MainNavbar />
+      {/* <MainNavbar /> */}
+      <h1>Brandy's countdown</h1>
+      <Countdown date={endDate} renderer={renderer} zeroPadTime={2}></Countdown>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/countries" component={Countries} />
