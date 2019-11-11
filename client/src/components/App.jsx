e0f40eaab0f9d18d06d7bd40dffe9dee131e2356
Guilherme Carmona <guilherme.carmona@outlook.com>
2019-11-11 15:59:58 +0000

    changed some random stuff

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -35,11 +35,11 @@ export default function App() {
       <Countdown date={endDate} renderer={renderer} zeroPadTime={2}></Countdown>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/countries" component={Countries} />
-        <Route path="/add-country" component={AddCountry} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/login" component={Login} />
-        <Route path="/secret" component={Secret} />
+        {/* <Route path="/countries" component={Countries} /> */}
+        {/* <Route path="/add-country" component={AddCountry} /> */}
+        {/* <Route path="/signup" component={Signup} /> */}
+        {/* <Route path="/login" component={Login} /> */}
+        {/* <Route path="/secret" component={Secret} /> */}
         <Route render={() => <h2>404</h2>} />
       </Switch>
     </div>
--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Home() {
   return (
     <div className="Home">
-      <h2>Home</h2>
-      <p>This is a sample project with the MERN stack</p>
+      {/* <h2>Home</h2> */}
+      {/* <p>This is a sample project with the MERN stack</p> */}
     </div>
   )
 }
