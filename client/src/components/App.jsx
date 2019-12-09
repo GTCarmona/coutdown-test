a386de21bf0e0b006ff46665657aa6e96771cbb5
Guilherme Carmona <guilherme.carmona@outlook.com>
2019-12-09 14:27:19 +0000

    added style and pictures

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,19 +1,12 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+// import { Route, Switch } from 'react-router-dom'
 // import MainNavbar from './MainNavbar'
-import Home from './pages/Home'
-import Countries from './pages/Countries'
-import AddCountry from './pages/AddCountry'
-import Secret from './pages/Secret'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
+import image1 from '../images/49523C1-R01-007A.Jpg'
 import Countdown from 'react-countdown-now'
 
 export default function App() {
   const Completionist = () => <span>TIME TO COME BACK BRIDGETTE!</span>
   const endDate = new Date('2020-01-29T03:24:00')
-  // var date2 = new Date('1995-12-17T03:24:00');
-  // Sun Dec 17 1995 03:24:00 GMT...
 
   const renderer = ({ days, hours, minutes, seconds, completed }) => {
     if (completed) {
@@ -21,27 +14,39 @@ export default function App() {
       return <Completionist />
     } else {
       // Render a countdown
+      const header = 'Only more'
+      const footer = 'To see your best mates again! '
+
+      if (50 >= days >= 40) {
+        header = 'Only more'
+        footer = 'To see your best mates again! '
+      }
       return (
-        <span>
-          Days: {days} Hours:{hours} Minutes: {minutes} Seconds:{seconds}
-        </span>
+        <div className="timer">
+          <span>{header} </span>
+          <span>
+            {days}Days {hours}H
+          </span>
+          <br />
+          <span>
+            {minutes}min {seconds} seconds
+          </span>
+          <br />
+
+          <span>{footer} </span>
+
+          <div className="image">
+            <img src={image1} alt="" />
+          </div>
+        </div>
       )
     }
   }
   return (
-    <div className="App">
+    <div className="Countdown">
       {/* <MainNavbar /> */}
-      <h1>Brandy's countdown</h1>
+      <h1>Bridgette's countdown</h1>
       <Countdown date={endDate} renderer={renderer} zeroPadTime={2}></Countdown>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        {/* <Route path="/countries" component={Countries} /> */}
-        {/* <Route path="/add-country" component={AddCountry} /> */}
-        {/* <Route path="/signup" component={Signup} /> */}
-        {/* <Route path="/login" component={Login} /> */}
-        {/* <Route path="/secret" component={Secret} /> */}
-        <Route render={() => <h2>404</h2>} />
-      </Switch>
     </div>
   )
 }
